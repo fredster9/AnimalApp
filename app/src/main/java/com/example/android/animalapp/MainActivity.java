e77cff2acbaa7585ef403b6e16c2aaa1f5cbd857
Fred D <10tonfred@gmail.com>
2017-07-19 08:33:37 -0400

    1. Updated to v 25.0.0 2. Add recycler view class 3. Added recycler view to layout

--- app/src/main/java/com/example/android/animalapp/MainActivity.java
+++ app/src/main/java/com/example/android/animalapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -42,4 +44,35 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
+}
+
+public class Contact {
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online) {
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline () {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2 ));
+        }
+
+        return contacts;
+    }
 }
