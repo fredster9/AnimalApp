5f28d3e8149b18a371b97274c9bb60476a6bbb6d
Fred D <10tonfred@gmail.com>
2017-07-19 21:54:24 -0400

    Adding in more reycler adapter stuff

---
+++ app/src/main/java/com/example/android/animalapp/ContactsAdapter.java
@@ -0,0 +1,37 @@
+package com.example.android.animalapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+// From https://github.com/codepath/android_guides/wiki/Using-the-RecyclerView
+
+// Create basic adapter extending from RecyclerView.Adapter
+// Specify customer ViewHolder to give access to our views
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+
+    // Provide direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+        public Button messageButton;
+
+        // Create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable
+            // used to access the context from any ViewHolder instance
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+    }
+
+}
+
+
